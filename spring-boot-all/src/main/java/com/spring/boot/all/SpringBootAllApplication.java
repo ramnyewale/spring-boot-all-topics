1850467478e4c66e833b6eaaea7094f674863d77
ram yewale <ramn.yewale@gmail.com>
2023-03-03 21:42:55 +0530

    First commit

---
+++ spring-boot-all/src/main/java/com/spring/boot/all/SpringBootAllApplication.java
@@ -0,0 +1,14 @@
+package com.spring.boot.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAllApplication.class, args);
+		System.out.println("Application started...!");
+	}
+
+}
