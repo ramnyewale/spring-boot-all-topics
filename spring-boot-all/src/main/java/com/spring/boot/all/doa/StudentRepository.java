055e2bf33463336c0d904bd61e2901e59c3c0900
Ram Yewale <ramn.yewale@gmail.com>
2024-03-16 01:08:47 +0530

    chore: Spring data jpa JPQL, Named & Native Queries.

--- spring-boot-all/src/main/java/com/spring/boot/all/doa/StudentRepository.java
+++ spring-boot-all/src/main/java/com/spring/boot/all/doa/StudentRepository.java
@@ -2,9 +2,30 @@ package com.spring.boot.all.doa;
 
 import com.spring.boot.all.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
+    Student findByGuardianName(String guardianName);
+
+    Student findStudentByName(String studentName);
+
+    Student findStudentByEmail(String email);
+
+    @Query(value = "Select s from Student s where s.email = ?1")
+    Student findStudentByEmailJpql(String mail);
+
+    @Query(value = "Select s from Student s where s.name = :studentName")
+    Student getStudentByNamedParam(@Param("studentName") String studentName);
+
+/*    @Modifying
+    @Transactional
+    @Query(
+            value = " update student s SET s.email_address = ?2 where s.student_name = ?1",
+            nativeQuery = true
+    )
+    Student updateStudentByName(String name, String mail);*/
 }
